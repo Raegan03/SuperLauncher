55f7f7170acb10c7c2f3ad3b0db5c4173a5b369b
Marcin434 <marcin.putniorz@gmail.com>
2019-11-18 16:46:23 +0100

    Project name typo fixed

--- SuperLuncherWPF/App.xaml.cs
+++ SuperLuncherWPF/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace SuperLuncherWPF
+namespace SuperLauncherWPF
 {
     /// <summary>
     /// Logika interakcji dla klasy App.xaml
--- SuperLuncherWPF/Properties/Resources.Designer.cs
+++ SuperLuncherWPF/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SuperLuncherWPF.Properties
+namespace SuperLauncherWPF.Properties
 {
 
 
