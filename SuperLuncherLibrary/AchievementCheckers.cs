fc841a4ff9a585194398c0124e6f0fa1b93440c8
Raegan <jan.gajewski3@gmail.com>
2020-01-24 00:07:55 +0100

    Achievements checker interface

---
+++ SuperLuncherLibrary/AchievementCheckers.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using SuperLauncher.Data;
+
+namespace SuperLauncher
+{
+    internal class FirstAchievementChecker : IAchievementChecker
+    {
+        public int AchievementID => 1;
+
+        public bool ValidateAchievement(ApplicationRuntimeData applicationData, List<SessionRuntimeData> sessionsData)
+        {
+            return sessionsData.Count > 0;
+        }
+    }
+}
---
+++ SuperLuncherLibrary/IAchievementChecker.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using SuperLauncher.Data;
+
+namespace SuperLauncher
+{
+    public interface IAchievementChecker
+    {
+        int AchievementID { get; }
+        bool ValidateAchievement(ApplicationRuntimeData applicationData, List<SessionRuntimeData> sessionsData);
+    }
+}
