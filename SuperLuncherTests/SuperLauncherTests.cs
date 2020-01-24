8f936c66245affd3a5282dade66bddfdb6b092f8
Drige300 <mariusz.kaczmarczyk@microsoft.wsei.edu.pl>
2020-01-24 20:04:30 +0100

    Tests work in progress

---
+++ SuperLuncherTests/SuperLauncherTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuperLauncher;
+
+namespace SuperLauncherTests
+{
+    [TestClass]
+    public class SuperLauncherTests
+    {
+        [TestMethod]
+        public void Test_SuperLaucher_Init()
+        {
+            var superLauncher = new SuperLauncher.SuperLauncher(null);
+
+            Assert.AreEqual(superLauncher.AchievementsCheckers.Count, superLauncher.CurrentApplicationAchievements.Count);
+            Assert.IsNotNull(superLauncher.ApplicationsData);
+            Assert.IsNotNull(superLauncher.SessionsData);
+
+            if(superLauncher.ApplicationsData.Count > 0)
+            {
+                Assert.IsFalse(superLauncher.IsApplicationsListEmpty);
+                Assert.AreEqual(superLauncher.CurrentApplicationData.AppGUID, superLauncher.ApplicationsData[0].AppGUID);
+
+                if(superLauncher.SessionsData.Count > 0)
+                {
+                    foreach (var session in superLauncher.SessionsData)
+                    {
+                        Assert.AreEqual(superLauncher.CurrentApplicationData.AppGUID, session.AppGUID);
+                    }
+                }
+            }
+            else
+            {
+                Assert.IsTrue(superLauncher.IsApplicationsListEmpty);
+                Assert.IsNotNull(superLauncher.CurrentApplicationData);
+            }
+        }
+    }
+}
